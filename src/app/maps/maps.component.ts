dd515931d76b44db6678a95cca49c7bf84fee6a5
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-04-20 07:19:58 +0900

    add a line as hole

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import {
+  GoogleMap, MapInfoWindow, MapMarker, MapPolyline,
+} from '@angular/google-maps';
+
+var HOLES = [
+  {
+    path: [
+      {lat: 34.787078, lng: 137.325119},
+      {lat: 34.787449, lng: 137.324232},
+    ]
+  }
+];
 
 @Component({
   selector: 'app-maps',
@@ -16,8 +27,10 @@ export class MapsComponent implements OnInit {
   markerPositions: google.maps.LatLngLiteral[] = [];
   zoom = 18;
   display?: google.maps.LatLngLiteral;
+  holes: Array<google.maps.LatLng[]> = [];
 
   constructor(
+    private ngZone: NgZone,
     private el: ElementRef,
   ) { }
 
@@ -25,6 +38,30 @@ export class MapsComponent implements OnInit {
     const rect = this.el.nativeElement.getBoundingClientRect();
     this.width = rect.width;
     this.height = rect.height;
+
+    HOLES.forEach(hole => {
+      const path: google.maps.LatLng[] = [];
+      hole.path.forEach(point => {
+        path.push(new google.maps.LatLng(point.lat, point.lng));
+      });
+      this.holes.push(path);
+    })
+  }
+
+  // get path() {
+  //   const path: google.maps.LatLng[] = [];
+  //   path.push(new google.maps.LatLng(holeCoordinates[0][0], holeCoordinates[0][1]));
+  //   path.push(new google.maps.LatLng(holeCoordinates[1][0], holeCoordinates[1][1]));
+  //   return path;
+  // }
+
+  get holeLineOptions() {
+    return {
+      // geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    };
   }
 
   addMarker(event: google.maps.MouseEvent) {
