3262aaef4cf4ec76fa2c310b42616727c47b3bda
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-05-16 23:21:36 +0900

    update EventTableDataSource with MatTableDataSource

--- src/app/event-table/event-table-datasource.ts
+++ src/app/event-table/event-table-datasource.ts
@@ -1,9 +1,4 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { map, tap, finalize } from 'rxjs/operators';
-import { Observable, of as observableOf, merge, BehaviorSubject } from 'rxjs';
-
+import { MatTableDataSource } from '@angular/material/table';
 import { CourseService, EventData } from '../course-service';
 export { EventData };
 
@@ -12,22 +7,11 @@ export { EventData };
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class EventTableDataSource extends DataSource<EventData> {
-  private readonly _loading: BehaviorSubject<boolean>;
-  get loading() { return this._loading.value; }
-  set loading(state: boolean) { this._loading.next(state); }
+export class EventTableDataSource extends MatTableDataSource<EventData> {
+  loading = true;
 
-  private readonly _data: BehaviorSubject<EventData[]>;
-  get data() { return this._data.value; }
-  set data(events: EventData[]) { this._data.next(events); }
-
-  paginator: MatPaginator;
-  sort: MatSort;
-
-  constructor(private readonly courseService: CourseService) {
+  constructor(private readonly remote: CourseService) {
     super();
-    this._loading = new BehaviorSubject<boolean>(true);
-    this._data = new BehaviorSubject<EventData[]>([]);
   }
 
   /**
@@ -35,61 +19,13 @@ export class EventTableDataSource extends DataSource<EventData> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<EventData[]> {
+  connect() {
     this.loading = true;
-    return this.courseService.getEvents().pipe(
-      tap(data => this.data = data),
-      finalize(() => this.loading = false)
+    this.remote.getEvents().subscribe(
+      events => this.data = events,
+      err => console.log(err),
+      () => this.loading = false
     );
-    // // Combine everything that affects the rendered data into one update
-    // // stream for the data-table to consume.
-    // const dataMutations = [
-    //   observableOf(this.data),
-    //   this.paginator.page,
-    //   this.sort.sortChange
-    // ];
-    //
-    // return merge(...dataMutations).pipe(
-    //   map(() => return this.getPagedData(this.getSortedData([...this.data]))
-    // ));
+    return super.connect();
   }
-
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
-  disconnect() {}
-
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getPagedData(data: EventData[]) {
-    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-    return data.splice(startIndex, this.paginator.pageSize);
-  }
-
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  // private getSortedData(data: EventData[]) {
-  //   if (!this.sort.active || this.sort.direction === '') {
-  //     return data;
-  //   }
-  //
-  //   return data.sort((a, b) => {
-  //     const isAsc = this.sort.direction === 'asc';
-  //     switch (this.sort.active) {
-  //       case 'name': return compare(a.name, b.name, isAsc);
-  //       case 'id': return compare(+a.id, +b.id, isAsc);
-  //       default: return 0;
-  //     }
-  //   });
-  // }
-}
-
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
