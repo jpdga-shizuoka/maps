727b73d7786230488907eebb1d5d92468bd9a23c
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-10-28 08:17:53 +0900

    a workaround for running on mobile device

--- src/app/print.service.ts
+++ src/app/print.service.ts
@@ -1,15 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { EventId, CourseId, TeeType } from './models';
 
+//
+//  a workaround for running on mobile device.
+//  @see https://github.com/IdanCo/angular-print-service/issues/3
+//
+let self: PrintService;
+
+function afterprintHandler(event: Event) {
+  if (!null) { return; }
+  self.isPrinting = false;
+  self.router.navigate(
+    [{ outlets: { print: null }}],
+    // { skipLocationChange: true }
+  );
+}
+
+function addAfterprintHandler(target: PrintService) {
+  self = target;
+  window.addEventListener('afterprint', afterprintHandler);
+}
+
+function removeAfterprintHandler() {
+  window.removeEventListener('afterprint', afterprintHandler);
+  self = null;
+}
+
+//
+//  @see https://github.com/IdanCo/angular-print-service
+//
 @Injectable({
   providedIn: 'root'
 })
-export class PrintService {
+export class PrintService implements OnDestroy {
   isPrinting = false;
 
-  constructor(private router: Router) { }
+  constructor(public readonly router: Router) {
+    addAfterprintHandler(this);
+  }
+
+  ngOnDestroy() {
+    removeAfterprintHandler();
+  }
 
   printDocument(
     documentName: string, eventId: EventId, courseId: CourseId, teeType: TeeType
@@ -25,12 +59,6 @@ export class PrintService {
   }
 
   onDataReady() {
-    setTimeout(() => {
-      window.print();
-      this.isPrinting = false;
-      this.router.navigate(['/', {
-        outlets: { print: null }
-      }]);
-    });
+    setTimeout(() => window.print());
   }
 }
